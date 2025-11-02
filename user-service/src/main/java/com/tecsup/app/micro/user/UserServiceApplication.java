d4b31ea484b71672b65452f01cd28dfaef4b80f4
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 14:42:15 -0500

    update

--- user-service/src/main/java/com/tecsup/app/micro/user/UserServiceApplication.java
+++ user-service/src/main/java/com/tecsup/app/micro/user/UserServiceApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
+    /**
+     *
+     * @param args
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
